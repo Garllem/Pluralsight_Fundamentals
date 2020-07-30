b0d7ddb132d79157e93ab864f8262bbe8a8ba655
Maikl <garllem@gmail.com>
2020-07-30 16:56:59 +0300

    DZ

---
+++ DZ/DZ 280.2/DZ 280.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DZ_280._2
+{
+    class Program
+    {
+        static int Summ2NatChisel(int znachenie)
+        {
+            /* int result = 0;
+            for (int i = 1; i <= znachenie; i++)
+                result += Convert.ToInt32(Math.Pow(i, 2));
+            return result;
+            */
+
+            if (znachenie <= 1)
+                return znachenie;
+            else
+                return Summ2NatChisel(znachenie - 1) + znachenie * znachenie;
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма квадратов натуральных чисел 6={0}", Summ2NatChisel(6));
+            Console.WriteLine("Сумма квадратов натуральных чисел 5={0}", Summ2NatChisel(5));
+        }
+    }
+}
---
+++ DZ/DZ 280.3/DZ 280.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DZ_280._3
+{
+    class Program
+    {
+        static int [] Metod(int [] a, int b)
+        {
+            int[] c = new int[b];
+            
+            for(int k=0;k<b;k++)
+            {
+                c[k]= a[k];
+            }
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работаем!");
+            int[] A = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            
+            A = Metod(A, 5);
+
+            for(int k=1;k<= A.Length;k++)
+            {
+                Console.Write(A[k-1]);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
